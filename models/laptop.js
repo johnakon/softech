4757873bd50c7c5278d4cba4b78b429921cbe5aa
John <john.akonya@gmail.com>
2020-02-05 11:13:24 +0300

    added the laptopAge and adjustPrice functions

--- models/laptop.js
+++ models/laptop.js
@@ -1,5 +1,5 @@
 class Laptop{
-    constructor(lapHddSize, lapRam, lapCpuSpeed, lapBatteryLife, lapScreenSize, lapModel, lapMake, lapPrice, lapOs, lapSerialNum){
+    constructor(lapHddSize, lapRam, lapCpuSpeed, lapBatteryLife, lapScreenSize, lapModel, lapMake, lapPrice, lapOs, lapSerialNum, lapDateOfMake){
         this.hddSize = lapHddSize;
         this.ram = lapRam;
         this.cpuSpeed = lapCpuSpeed;
@@ -10,12 +10,13 @@ class Laptop{
         this.price = lapPrice;
         this.os = lapOs;
         this.serialNum = lapSerialNum
+        this.dom = lapDateOfMake
     }
 
     //adding a method to return the printed string
     // compose a 
     printableString(){
-        return `lap properties hddSize ${this.hddSize}:`
+        return `lap properties hddSize: ${this.hddSize} \n RAM size: ${this.ram} \n CPUSpeed: ${this.cpuSpeed} \n BatteryLife: ${this.batteryLife} \n ScreenSSize: ${this.screenSize} \n Laptop mdel: ${this.lapModel} \n Laptop amnufucturer: ${this.make} \n Laptop price: ${this.price} \n Laptop OS: ${this.os} \n Laptop serial num: ${this.serialNum} \n Date of maufuctury: ${this.dom}:`
     }
     checkRam(){
         if(typeof this.ram == "number"){
@@ -23,6 +24,22 @@ class Laptop{
         }else{
             return false;
         }
+    }
+    //other methods
+    //
+    laptopAge(){
+        var currentDate = new Date()
+        var currentYear = currentDate.getFullYear()
+        return `${currentYear} - ${this.dom.getFullYear()}`
+    }
+
+    adjustPrice(){
+       var currentLapTopAge = this.laptopAge()
+    //    var discount = 0;
+       if(currentLapTopAge > 3){
+           var discount = (this.price * 0.3)
+       }
+       return `${this.price} - ${discount}`
     }
 }
 
