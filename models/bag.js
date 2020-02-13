54055b5dca6b4d8055dac51388211198a3993342
John <john.akonya@gmail.com>
2020-02-13 16:01:40 +0300

    bag class

---
+++ models/bag.js
@@ -0,0 +1,9 @@
+class Bag{
+    constructor(bagColor, bagSize, bagWeight, bagPrice, bagModel){
+        this.color = bagColor;
+        this.size = bagSize;
+        this.weight = bagWeight;
+        this.price = bagPrice;
+        this.model = bagModel;
+    }
+}
